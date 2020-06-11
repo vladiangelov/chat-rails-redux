02e5806bad4fb053fd981fbac2976b1200f167c8
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-06-11 20:21:30 +0100

    Initial commit - rails 5 new + devise, also added channel and message activerecord models and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+channels = ["general", "paris", "react", "london", "sofia"]
+channels.each do |channel_name|
+  new_channel = Channel.new(name: channel_name)
+  new_channel.save
+end
+
+emails = [ "vladi@gmail.com", "david@gmail.com", "meggie@gmail.com", "john@gmail.com", "susan@gmail.com" ]
+emails.each do |email_address|
+  new_user = User.new(email: email_address, password: "password")
+  new_user.save
+end
+
+50.times do
+  user = User.find(rand(1..5))
+  channel = Channel.find(rand(1..5))
+  message = Message.new(content: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.")
+  message.user = user
+  message.channel = channel
+  message.save
+end
